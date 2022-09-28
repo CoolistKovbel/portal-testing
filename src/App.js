1798eee38adeb69dea302519598f7d50eef509d5
coolistkovbel <coolistkovbel@gmail.com>
2022-09-28 03:20:00 -0400

    added the etheruem client to check if user is connected to app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
-import * as React from "react";
-import { ethers } from "ethers";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 export default function App() {
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("make Sure you have metamask");
+      } else {
+        console.log("there is an ethereum object", window);
+      }
+
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an account: ", account);
+        setCurrentAccount(account);
+      } else {
+        console.log("No account found");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const wave = () => {};
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
 
   return (
     <div className="app">
       <header className="mainHeader">
         <h2>Portal</h2>
+        {currentAccount ? (
+          <p>User Account: {currentAccount}</p>
+        ) : (
+          <p>Connect account</p>
+        )}
       </header>
       <div className="test">
         <p>This is basically a small test to be able to send a wave</p>
